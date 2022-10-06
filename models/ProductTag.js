f5edf5a8082ca1c04416a5304c998e9da768a6bc
Sarah Prantis <sprantis@gmail.com>
2022-10-05 20:18:57 -0400

    defined columns for models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,3 +1,4 @@
+// Referencing code from Module 13 Mini Project
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
@@ -7,6 +8,28 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+        unique: false
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+        unique: false
+      }
+    },
   },
   {
     sequelize,
