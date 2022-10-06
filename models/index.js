9c9e4c29c42c4ad364147b8c7c3cf9822cddf71f
Sarah Prantis <sprantis@gmail.com>
2022-10-05 20:19:41 -0400

    defined association methods on Sequelize models

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
+// Referencing code from Module 13 Mini Project
 // import models
 const Product = require('./Product');
 const Category = require('./Category');
@@ -5,12 +6,33 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  foreignKey: 'product_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  foreignKey: 'tag_id',
+});
 
 module.exports = {
   Product,
